ba3d83894213fa11d0ae27dbc96b1c0f69de90d8
Denise Boccon <jakomy.joel@hotmail.it>
2015-10-11 20:09:58 +0200

    COMMITTO L'INTERO PROGETTO

---
+++ SRC/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "bmp.h"
+
+
+
+int main(int argc, char* argv[]){
+
+    printf("\n\nBENVENUTO NEL NOSTRO PROGRAMMA!\n");
+
+    menu();
+
+
+
+    return 0;
+}
+
